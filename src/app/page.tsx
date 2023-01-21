968ad56e33b568c1f276ae5e2d5b4640b4762f51
ckibetrono <ckibetrono@gmail.com>
2023-01-22 01:44:51 +0300

    added a dark mode/ light mode button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,32 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ArrowDown } from "./arrowDown";
+import { Moon } from "./Moon"; 
+import { Sun } from "./Sun";
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
   const [selectValue, setSelectValue] = useState("Option 1");
+
+  const size="24px";
+  const [darkMode, setDarkMode] = useState<boolean | undefined>(undefined);
+
+  const switchMode = () => {
+    setDarkMode(!darkMode);
+  }
+  
+  useEffect(() => {
+    if (darkMode) {
+      localStorage.setItem("darkMode", "true");
+      window.document.documentElement.classList.add("dark");
+    } else if (darkMode == false) {
+      localStorage.setItem("darkMode", "false");
+      window.document.documentElement.classList.remove("dark");
+    } else {
+      setDarkMode(localStorage.getItem("darkMode") === "true");
+    }
+  }, [darkMode]);
 
   const updateValue = (value: string) => {
     setSelectValue(value);
@@ -14,6 +35,13 @@ export default function Home() {
 
   return (
     <div className="h-screen Parent p-4">
+      <header className="flex items-center justify-end w-full p-4">
+        <div className="transition cursor-pointer hover:text-blue-600" onClick={switchMode}>
+          {!darkMode ? (<Moon width={size} height={size}/>):(<Sun width={size} height={size} />)}
+        </div>
+      </header>
+
+
       <h1>This is a title</h1>
       <h2>This is a subtitle</h2>
       <p className="text-base">This is a paragraph</p>
